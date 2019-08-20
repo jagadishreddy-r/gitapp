49a591795c48daeb47f830cdc49cb840943fd323
krishnavamsi39 <krishnavamsi495495@gmail.com>
2019-08-20 12:28:41 +0530

    [green state] for repomodel

--- src/Stores/Models/RepoModel/index.js
+++ src/Stores/Models/RepoModel/index.js
@@ -3,6 +3,11 @@ class RepoModel {
   name;
   stargazers_count;
   forks_count;
-  constructor(id, name, stargazers_count, forks_count) {}
+  constructor(id, name, stargazers_count, forks_count) {
+    this.id = id;
+    this.name = name;
+    this.stargazers_count = stargazers_count;
+    this.forks_count = forks_count;
+  }
 }
 export default RepoModel;
