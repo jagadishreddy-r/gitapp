4a1b55c70af3ee47de978a30f84ef2dfd26d396e
krishnavamsi39 <krishnavamsi495495@gmail.com>
2019-08-22 16:46:03 +0530

    solved double tap issue

--- src/Utils/RouterActions/index.js
+++ src/Utils/RouterActions/index.js
@@ -2,7 +2,8 @@ import {Actions} from 'react-native-router-flux';
 import translate from '../../Utils/language.utils';
 class RouterActions {
   goRepoScreen = user => {
-    Actions.repos({user, title: user.userName + translate('Repos')});
+    if (Actions.currentScene !== 'repos')
+      Actions.repos({user, title: user.userName + translate('Repos')});
   };
 }
 export default RouterActions;
